0dc852e92d3fb47f08623acaa8532f9a309e8fee
Gabrielnm7 <gabrielnm7@hotmail.com>
2024-10-22 10:58:36 -0300

    Add: eliminar

--- Taller/taller4/src/main/java/aed/ABB.java
+++ Taller/taller4/src/main/java/aed/ABB.java
@@ -5,17 +5,17 @@ import java.util.*;
 // Todos los tipos de datos "Comparables" tienen el método compareTo()
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
-    // Agregar atributos privados del Conjunto
+    // Atributos privados del Conjunto
     private Nodo _raiz;
     private int _cardinal;
 
     private class Nodo {
-        // Agregar atributos privados del Nodo
+        // Atributos privados del Nodo
         T valor;
         Nodo izq;
         Nodo der;
         Nodo padre;
-        // Crear Constructor del nodo
+        // Constructor del nodo
         Nodo(T valor) {
             this.valor = valor;
             this.izq = null;
@@ -97,7 +97,69 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     }
 
     public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo actual = this._raiz;
+        while (actual != null){
+            // Si es menor, voy a la izquierda
+            if (elem.compareTo(actual.valor) < 0){
+                actual = actual.izq;
+            }
+            // Si es mayor, voy a la derecha
+            else if (elem.compareTo(actual.valor) > 0){
+                actual = actual.der;
+            }
+            // Si lo encuentro 
+            else if (elem.compareTo(actual.valor) == 0){
+                // 1er Caso: No tiene hijos -> Elimino el nodo
+                if (actual.izq == null && actual.der == null){
+                    if (actual.padre == null){
+                        this._raiz = null;
+                        // si soy el de la izquierda
+                    } else if (actual.padre.izq == actual){
+                        actual.padre.izq = null;
+                        // si soy el de la derecha
+                    } else {
+                        actual.padre.der = null;
+                    }
+                }
+                
+                // 2do Caso: Tiene un hijo -> Reemplazo el nodo por su hijo
+                else if (actual.izq == null || actual.der == null){
+                    Nodo hijo = (actual.izq != null) ? actual.izq : actual.der;
+                    if (actual.padre == null){
+                        this._raiz = hijo;
+                    } else if (actual.padre.izq == actual){
+                        actual.padre.izq = hijo;
+                    } else {
+                        actual.padre.der = hijo;
+                    }
+                    hijo.padre = actual.padre;
+                }
+
+                // 3er Caso: Tiene dos hijos -> Reemplazo por el minimo del subarbol derecho
+                else {
+                    Nodo minimo = actual.der;
+                    while (minimo.izq != null){
+                        minimo = minimo.izq;
+                    }
+                    // Reemplazo el valor del nodo actual por el valor del minimo
+                    actual.valor = minimo.valor;
+                    // Eliminamos el minimo, tengo 2 casos: si tiene hijo o no
+                    // Si tiene hijo, lo subo
+                    if (minimo.der != null){
+                        minimo.padre.izq = minimo.der;
+                        minimo.der.padre = minimo.padre;
+                    }
+                    // Si no tiene hijo, lo elimino
+                    else {
+                        minimo.padre.izq = null;
+                    }
+                }
+            // Le resto uno al cardinal
+            this._cardinal--;
+            // Salgo del ciclo
+            break;
+            }
+        }
     }
 
     public String toString(){
@@ -105,7 +167,7 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     }
 
     private class ABB_Iterador implements Iterador<T> {
-        private Nodo _actual;
+        private Nodo _actual = minimo(); // Necesito que minimo devuelva el nodo minimo, no el valor minimo
 
         public boolean haySiguiente() {            
             throw new UnsupportedOperationException("No implementada aun");
